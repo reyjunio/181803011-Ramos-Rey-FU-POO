fc21dd1523a96c836f076939915ba3f1fca0bdba
Rey Junior Ramos Flores <rey.ramos1111@uppuebla.edu.mx>
2020-05-19 09:58:48 -0500

    Vaariables 19-05-20

---
+++ TipoVariables/src/main/java/TipoVariables.java
@@ -0,0 +1,39 @@
+
+public class TipoVariables {
+    public static void main(String[] args) {
+        //lengusje fuertemente tipsdo
+        //tipos de variable que   siempre mntendra ese tipo
+        
+        int  a=10, b=2;
+        
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        
+        
+        //decimales
+        float pi=3.14f;
+        double pi2=3.14;
+        
+        boolean bndera=true;
+        
+        
+        //ingresar caracteres
+        char letra='a';
+        
+        String palabra="puedes poner mensaje largo";
+        System.out.println(palabra);
+        
+        //concatenar
+        System.out.println(palabra +"y esto es un mensaje de ello");
+        
+        System.out.println("este es el valor de pi"+pi);
+        System.out.println("este es el valor del circulo"+pi*a);
+        System.out.println(pi*a+"este es el valor del circulo");
+           
+        
+    }
+    
+}
