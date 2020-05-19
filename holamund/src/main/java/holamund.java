4268c3948c40b54a63bb73b14c2a4f905c767339
Rey Junior Ramos Flores <rey.ramos1111@uppuebla.edu.mx>
2020-05-19 09:52:21 -0500

    hola y nombre 19-05-20

---
+++ holamund/src/main/java/holamund.java
@@ -0,0 +1,10 @@
+
+public class holamund {
+    public static void main(String[] args) {
+        System.out.println("hola");
+        System.out.println("rey junior ramos flores");
+        
+    }
+    
+    
+}
