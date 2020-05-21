2f17c0de5243eace336939ecf3a9581ae6e6900a
Rey Junior Ramos Flores <rey.ramos1111@uppuebla.edu.mx>
2020-05-21 08:37:07 -0500

    Area Cuadrado

---
+++ AreaCuadrado/src/main/java/AreaCuadrado.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+/*
+un programa calcular el area de un cuadrado
+ */
+public class AreaCuadrado {
+    public static void main(String[] args) {
+      Scanner leer=new Scanner(System.in);
+      System.out.println("Introduce el lado del cuadrado.; ");
+      double numero=leer.nextDouble();
+      
+        System.out.println("el area del cuadrado es"+numero*numero);
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
