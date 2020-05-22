e8f972862de574f43b737f411927ca3ba59cb3e7
Rey Junior Ramos Flores <rey.ramos1111@uppuebla.edu.mx>
2020-05-22 16:10:05 -0500

    Porcentaje

---
+++ CalcularPorcentaje/src/main/java/CalcularPorcentaje.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+/*
+calcule el porcentaje de reprobación de un grupo, 
+a partir de la cantidad total de alumnos que presentaron un examen y el número 
+de alumnos que lo reprobaron
+ */
+public class CalcularPorcentaje {
+    public static void main(String[] args) {
+        Scanner numero = new Scanner(System.in);
+        
+        System.out.print("Cantidad de alumnos que presentaron un examen : ");
+        double alum=numero.nextDouble();
+        
+        System.out.print("Cantidad de alumnos que reprobaron :  ");
+        double repro=numero.nextDouble();
+        
+        System.out.println((repro/alum)*100+"%");
+        
+    }
+}
